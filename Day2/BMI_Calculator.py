22337e9871496f09feb6ebd654f8fe89c9377dd8
Emmanuel Mabhena <52994592+emmanuelmabhena@users.noreply.github.com>
2025-05-09 11:16:30 +0200

    BMI Calculator done

--- Day2/BMI_Calculator.py
+++ Day2/BMI_Calculator.py
@@ -1,1 +1,9 @@
+print("Welcome to BMI calculator")
 
+Weight = input("What is your weight in Kilograms? ")
+Weight = float(Weight)
+
+Height = input("What is your height in Metres? ")
+Height = float(Height)
+
+print(f"Your BMI is {Weight/Height**2:.2f}")
