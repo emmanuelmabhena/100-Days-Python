465405a522907fa1f0c86d1128b313987850bee3
Emmanuel Mabhena <52994592+emmanuelmabhena@users.noreply.github.com>
2025-05-09 11:08:30 +0200

    Tip Calculator done

--- Day2/Tip_Calculator.py
+++ Day2/Tip_Calculator.py
@@ -1,1 +1,12 @@
+print("Welcome to the tip calculator!")
 
+Total_Amount = input("What was the total bill? ")
+Total_Amount = float(Total_Amount)
+
+Tip = input("How much tip would you like to give? 12, 10 or 15 ")
+Tip = float(Tip)
+
+People_Paying = input("How many people to split the bill ")
+People_Paying = float(People_Paying)
+
+print(f"Each person should pay ${(Total_Amount * Tip / 100) / People_Paying:.2f}")
